040559bf380539dfaeaf8c682d07f8a8e48858de
IgorVitula <36676091+IgorVitula@users.noreply.github.com>
2018-04-09 19:29:18 -0300

    Add files via upload

---
+++ Tela_Inicial.java
@@ -0,0 +1,34 @@
+package com.ntecprograms.samu;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Tela_Inicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela__inicial);
+    }
+
+
+    public void Login(View view){
+
+       Intent intent = new Intent(this,Tela_Login.class);
+        startActivity(intent);
+    }
+
+    public void Cadastrar(View view){
+        Intent Intent = new Intent(this,Tela_Cadastro.class);
+        startActivity(Intent);
+    }
+
+    public void Sobre(View view){
+
+
+        Intent Intent = new Intent(this,Tela_Sobre.class);
+        startActivity(Intent);
+    }
+}
