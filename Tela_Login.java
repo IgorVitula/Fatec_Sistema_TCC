38caa05cbb5915761473a5249c3d8947aff52eeb
IgorVitula <36676091+IgorVitula@users.noreply.github.com>
2018-04-24 22:07:58 -0300

    Add files via upload

---
+++ Tela_Login.java
@@ -0,0 +1,35 @@
+package com.ntecprograms.samu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Tela_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela__login);
+
+        Button Logar =(Button)findViewById(R.id.btnLogar);
+
+        final EditText Login =(EditText)findViewById(R.id.edtLogin);
+        final EditText Senha =(EditText)findViewById(R.id.edtSenha);
+        final EditText CpfLogin =(EditText)findViewById(R.id.edtCpfLogin);
+
+
+        Logar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Login.getText().length()==0 || Senha.getText().length()==0 || CpfLogin.getText().length() ==0){
+                    Login.setError("Campo Requirido");
+                    Senha.setError("Campo Requirido");
+                    CpfLogin.setError("Campo Requirido");
+                }
+            }
+        });
+    }
+}
